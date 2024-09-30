89afb21b6772c20f6081c5112aef2c51c5e9ac87
jleon2058 <arscaballerie@gmail.com>
2024-09-30 05:43:36 -0500

    Se remplazó la variable qty_done por quantity del sml y se redefinio el metodo _post() , para que se establezca a borrador todas las facturas

--- product_cost_invoice/models/models.py
+++ product_cost_invoice/models/models.py
@@ -5,6 +5,23 @@ from collections import defaultdict
 from odoo import api, fields, models, _
 from odoo.tools.float_utils import float_round, float_is_zero
 from odoo.exceptions import UserError
+from odoo.tools import (
+    date_utils,
+    email_re,
+    email_split,
+    float_compare,
+    float_is_zero,
+    float_repr,
+    format_amount,
+    format_date,
+    formatLang,
+    frozendict,
+    get_lang,
+    groupby,
+    index_exists,
+    is_html_empty,
+    create_index,
+)
 import logging
 logger = logging.getLogger(__name__)
 
@@ -31,14 +48,14 @@ class StockMove(models.Model):
             valued_move_lines = move._get_in_move_lines()
             # logger.warning("-------valued_move_lines-------")
             # logger.warning(valued_move_lines)
-            qty_done = 0
+            quantity = 0
             for valued_move_line in valued_move_lines:
-                qty_done += valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, move.product_id.uom_id)
+                quantity += valued_move_line.product_uom_id._compute_quantity(valued_move_line.quantity, move.product_id.uom_id)
                 # logger.warning("-------_compute_quantity-------")
                 # logger.warning(valued_move_line.qty_done)
                 # logger.warning(move.product_id.uom_id)
 
-            qty = forced_qty or qty_done
+            qty = forced_qty or quantity
             # logger.warning("-------forced_qty or qty_done-------")
             # logger.warning(forced_qty)
             # logger.warning(qty_done)
@@ -70,7 +87,7 @@ class StockMove(models.Model):
                     # logger.warning("------new_std_price-----")
                     # logger.warning(amount_unit)
                     # logger.warning(new_std_price)
-            tmpl_dict[move.product_id.id] += qty_done
+            tmpl_dict[move.product_id.id] += quantity
             # Write the standard price, as SUPERUSER_ID because a warehouse manager may not have the right to write on products
             move.product_id.with_company(move.company_id.id).with_context(disable_auto_svl=True).sudo().write({'standard_price': new_std_price})
             std_price_update[move.company_id.id, move.product_id.id] = new_std_price
@@ -149,7 +166,7 @@ class AccountMove(models.Model):
             valued_move_lines = line._get_in_move_lines()
             valued_quantity = 0
             for valued_move_line in valued_move_lines:
-                valued_quantity += valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, line.product_id.uom_id)
+                valued_quantity += valued_move_line.product_uom_id._compute_quantity(valued_move_line.quantity, line.product_id.uom_id)
             for i in valuation:
                 i.write({
                     'value': line.company_id.currency_id.round(unit_price * i.quantity),
@@ -215,7 +232,7 @@ stock.valuation.layer
         for line in move_lines:
             line.product_price_update_before_done(
                 forced_qty=sum(
-                    [valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, line.product_id.uom_id)
+                    [valued_move_line.product_uom_id._compute_quantity(valued_move_line.quantity, line.product_id.uom_id)
                      for valued_move_line in line._get_in_move_lines()]) * -1,
                 is_new=True,
             )
@@ -230,7 +247,7 @@ stock.valuation.layer
                 self.update_stock_move_line(line)
             move_lines[-1].product_price_update_before_done(
                 forced_qty=sum(
-                    [valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, line.product_id.uom_id)
+                    [valued_move_line.product_uom_id._compute_quantity(valued_move_line.quantity, line.product_id.uom_id)
                      for valued_move_line in line._get_in_move_lines()]) * -1,
                 is_new=True,
             )
@@ -244,6 +261,170 @@ stock.valuation.layer
 
         return res
 
+    def _post(self, soft=True):
+        """Post/Validate the documents.
+
+        Posting the documents will give it a number, and check that the document is
+        complete (some fields might not be required if not posted but are required
+        otherwise).
+        If the journal is locked with a hash table, it will be impossible to change
+        some fields afterwards.
+
+        :param soft (bool): if True, future documents are not immediately posted,
+            but are set to be auto posted automatically at the set accounting date.
+            Nothing will be performed on those documents before the accounting date.
+        :return Model<account.move>: the documents that have been posted
+        """
+        if not self.env.su and not self.env.user.has_group('account.group_account_invoice'):
+            raise AccessError(_("You don't have the access rights to post an invoice."))
+
+        for invoice in self.filtered(lambda move: move.is_invoice(include_receipts=True)):
+            if (
+                invoice.quick_edit_mode
+                and invoice.quick_edit_total_amount
+                and invoice.currency_id.compare_amounts(invoice.quick_edit_total_amount, invoice.amount_total) != 0
+            ):
+                raise UserError(_(
+                    "The current total is %s but the expected total is %s. In order to post the invoice/bill, "
+                    "you can adjust its lines or the expected Total (tax inc.).",
+                    formatLang(self.env, invoice.amount_total, currency_obj=invoice.currency_id),
+                    formatLang(self.env, invoice.quick_edit_total_amount, currency_obj=invoice.currency_id),
+                ))
+            if invoice.partner_bank_id and not invoice.partner_bank_id.active:
+                raise UserError(_(
+                    "The recipient bank account linked to this invoice is archived.\n"
+                    "So you cannot confirm the invoice."
+                ))
+            if float_compare(invoice.amount_total, 0.0, precision_rounding=invoice.currency_id.rounding) < 0:
+                raise UserError(_(
+                    "You cannot validate an invoice with a negative total amount. "
+                    "You should create a credit note instead. "
+                    "Use the action menu to transform it into a credit note or refund."
+                ))
+
+            if not invoice.partner_id:
+                if invoice.is_sale_document():
+                    raise UserError(_("The field 'Customer' is required, please complete it to validate the Customer Invoice."))
+                elif invoice.is_purchase_document():
+                    raise UserError(_("The field 'Vendor' is required, please complete it to validate the Vendor Bill."))
+
+            # Handle case when the invoice_date is not set. In that case, the invoice_date is set at today and then,
+            # lines are recomputed accordingly.
+            if not invoice.invoice_date:
+                if invoice.is_sale_document(include_receipts=True):
+                    invoice.invoice_date = fields.Date.context_today(self)
+                elif invoice.is_purchase_document(include_receipts=True):
+                    raise UserError(_("The Bill/Refund date is required to validate this document."))
+
+        for move in self:
+            if move.state in ['posted', 'cancel']:
+                raise UserError(_('The entry %s (id %s) must be in draft.', move.name, move.id))
+            if not move.line_ids.filtered(lambda line: line.display_type not in ('line_section', 'line_note')):
+                raise UserError(_('You need to add a line before posting.'))
+            if not soft and move.auto_post != 'no' and move.date > fields.Date.context_today(self):
+                date_msg = move.date.strftime(get_lang(self.env).date_format)
+                raise UserError(_("This move is configured to be auto-posted on %s", date_msg))
+            if not move.journal_id.active:
+                raise UserError(_(
+                    "You cannot post an entry in an archived journal (%(journal)s)",
+                    journal=move.journal_id.display_name,
+                ))
+            if move.display_inactive_currency_warning:
+                raise UserError(_(
+                    "You cannot validate a document with an inactive currency: %s",
+                    move.currency_id.name
+                ))
+
+            if move.line_ids.account_id.filtered(lambda account: account.deprecated) and not self._context.get('skip_account_deprecation_check'):
+                raise UserError(_("A line of this move is using a deprecated account, you cannot post it."))
+
+        if soft:
+            # future_moves = self.filtered(lambda move: move.date > fields.Date.context_today(self))
+            # for move in future_moves:
+            #     if move.auto_post == 'no':
+            #         move.auto_post = 'at_date'
+            #     msg = _('This move will be posted at the accounting date: %(date)s', date=format_date(self.env, move.date))
+            #     move.message_post(body=msg)
+            to_post = self
+        else:
+            to_post = self
+
+        for move in to_post:
+            affects_tax_report = move._affect_tax_report()
+            lock_dates = move._get_violated_lock_dates(move.date, affects_tax_report)
+            if lock_dates:
+                move.date = move._get_accounting_date(move.invoice_date or move.date, affects_tax_report)
+
+        # Create the analytic lines in batch is faster as it leads to less cache invalidation.
+        to_post.line_ids._create_analytic_lines()
+
+        # Trigger copying for recurring invoices
+        to_post.filtered(lambda m: m.auto_post not in ('no', 'at_date'))._copy_recurring_entries()
+
+        for invoice in to_post:
+            # Fix inconsistencies that may occure if the OCR has been editing the invoice at the same time of a user. We force the
+            # partner on the lines to be the same as the one on the move, because that's the only one the user can see/edit.
+            wrong_lines = invoice.is_invoice() and invoice.line_ids.filtered(lambda aml:
+                aml.partner_id != invoice.commercial_partner_id
+                and aml.display_type not in ('line_note', 'line_section')
+            )
+            if wrong_lines:
+                wrong_lines.write({'partner_id': invoice.commercial_partner_id.id})
+
+        # reconcile if state is in draft and move has reversal_entry_id set
+        draft_reverse_moves = to_post.filtered(lambda move: move.reversed_entry_id and move.reversed_entry_id.state == 'posted')
+
+        to_post.write({
+            'state': 'posted',
+            'posted_before': True,
+        })
+
+        draft_reverse_moves.reversed_entry_id._reconcile_reversed_moves(draft_reverse_moves, self._context.get('move_reverse_cancel', False))
+        to_post.line_ids._reconcile_marked()
+
+        for invoice in to_post:
+            invoice.message_subscribe([
+                p.id
+                for p in [invoice.partner_id]
+                if p not in invoice.sudo().message_partner_ids
+            ])
+
+            if (
+                invoice.is_sale_document()
+                and invoice.journal_id.sale_activity_type_id
+                and (invoice.journal_id.sale_activity_user_id or invoice.invoice_user_id).id not in (self.env.ref('base.user_root').id, False)
+            ):
+                invoice.activity_schedule(
+                    date_deadline=min((date for date in invoice.line_ids.mapped('date_maturity') if date), default=invoice.date),
+                    activity_type_id=invoice.journal_id.sale_activity_type_id.id,
+                    summary=invoice.journal_id.sale_activity_note,
+                    user_id=invoice.journal_id.sale_activity_user_id.id or invoice.invoice_user_id.id,
+                )
+
+        customer_count, supplier_count = defaultdict(int), defaultdict(int)
+        for invoice in to_post:
+            if invoice.is_sale_document():
+                customer_count[invoice.partner_id] += 1
+            elif invoice.is_purchase_document():
+                supplier_count[invoice.partner_id] += 1
+            elif invoice.move_type == 'entry':
+                sale_amls = invoice.line_ids.filtered(lambda line: line.partner_id and line.account_id.account_type == 'asset_receivable')
+                for partner in sale_amls.mapped('partner_id'):
+                    customer_count[partner] += 1
+                purchase_amls = invoice.line_ids.filtered(lambda line: line.partner_id and line.account_id.account_type == 'liability_payable')
+                for partner in purchase_amls.mapped('partner_id'):
+                    supplier_count[partner] += 1
+        for partner, count in customer_count.items():
+            (partner | partner.commercial_partner_id)._increase_rank('customer_rank', count)
+        for partner, count in supplier_count.items():
+            (partner | partner.commercial_partner_id)._increase_rank('supplier_rank', count)
+
+        # Trigger action for paid invoices if amount is zero
+        to_post.filtered(
+            lambda m: m.is_invoice(include_receipts=True) and m.currency_id.is_zero(m.amount_total)
+        )._invoice_paid_hook()
+
+        return to_post
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -269,7 +450,7 @@ class ProductTemplate(models.Model):
         for line in move_lines:
             line.product_price_update_before_done(
                 forced_qty=sum(
-                    [valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, line.product_id.uom_id)
+                    [valued_move_line.product_uom_id._compute_quantity(valued_move_line.quantity, line.product_id.uom_id)
                      for valued_move_line in line._get_in_move_lines()]) * -1,
                 is_new=True,
             )
@@ -288,7 +469,7 @@ class ProductTemplate(models.Model):
                 line = move_lines.filtered(lambda x: x.product_id.id == product.id).sorted(lambda x: x.date)[-1]
                 line.product_price_update_before_done(
                     forced_qty=sum(
-                        [valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, line.product_id.uom_id)
+                        [valued_move_line.product_uom_id._compute_quantity(valued_move_line.quantity, line.product_id.uom_id)
                          for valued_move_line in line._get_in_move_lines()]) * -1,
                     is_new=True,
                 )
