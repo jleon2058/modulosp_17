ec330c5b09db66581ef49b8c70cf4dd6b992ad7c
jleon2058 <arscaballerie@gmail.com>
2024-12-01 06:19:28 -0500

    loger a dv_analytic_account_target

--- dv_analytic_account_target_move/models/account_move.py
+++ dv_analytic_account_target_move/models/account_move.py
@@ -1,15 +1,20 @@
 from odoo.exceptions import UserError
 from odoo import api, fields, models, _
-
+import logging
+logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    logger.warning("-----------xxxxx------------")
     def _post(self, soft=True):
+        logger.warning("--------------posttarge------------")
         for move in self: #.filtered(lambda m: m.move_type not in ['out_invoice', 'out_receipt'])
             for line in move.line_ids:
+                logger.warning("-------------for line------------")
                 line.validate_analytic_account()
                 if line.account_target_move_type == 'entry' and (line.debit + line.credit) != 0:
+                    logger.warning("------------if-for line------------")
                     debit_target_account_id = line.account_id.debit_target_account_id
                     credit_target_account_id = line.account_id.credit_target_account_id
                     if not debit_target_account_id or not credit_target_account_id:
@@ -18,6 +23,7 @@ class AccountMove(models.Model):
                     line._create_target_move_lines(
                         debit_target_account_id, credit_target_account_id)
                 elif line.account_target_move_type == 'analytic' and line.analytic_account_id and (line.debit + line.credit) != 0:
+                    logger.warning("------------elif-for line------------")
                     debit_target_account_id = line.analytic_account_id.debit_target_account_id
                     credit_target_account_id = line.analytic_account_id.credit_target_account_id
                     if debit_target_account_id and credit_target_account_id:
